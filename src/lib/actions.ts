5805700d8bafe44c2397d8413271b4e6f76fb965
renan-leonel <renanleonelpro@gmail.com>
2024-02-02 16:15:53 -0300

    feat: zod schema validation

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,11 +2,18 @@
 
 import { Resend } from 'resend';
 import { VercelInviteUserEmail } from '@/components/email-template';
+import { schema } from './schema';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail(formData: FormData) {
 	const email = formData.get('email') as string;
+
+	const validate = schema.safeParse({ email });
+
+	if (!validate.success) {
+		return;
+	}
 
 	try {
 		const data = await resend.emails.send({
