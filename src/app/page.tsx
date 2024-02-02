aa346243ce7dd8378531cef93504b9bfe2ebf5ca
renan-leonel <renanleonelpro@gmail.com>
2024-02-02 15:41:52 -0300

    feat: server action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { sendEmail } from '@/lib/actions';
 
 export default function Home() {
 	return (
-		<form className='flex gap-4'>
-			<Input placeholder='email' />
+		<form action={sendEmail} className='flex gap-4'>
+			<Input name='email' placeholder='email' />
 			<Button type='submit' variant='secondary'>
 				send
 			</Button>
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,18 +1,22 @@
+'use server';
+
 import { Resend } from 'resend';
 import { EmailTemplate } from '@/components/email-template';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function GET() {
+export async function sendEmail(formData: FormData) {
+	const email = formData.get('email') as string;
+
 	try {
 		const data = await resend.emails.send({
 			from: 'Acme <onboarding@resend.dev>',
-			to: [''],
+			to: email,
 			subject: 'Hello world',
 			react: EmailTemplate({ firstName: 'Renan' }),
 		});
 
-		return Response.json(data);
+		return data;
 	} catch (error) {
 		return Response.json({ error });
 	}
